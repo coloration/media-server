7e47b89095446d76d0b3f538845ec41fd88c4a4d
Binyu <binyu1231@gmail.com>
2024-07-29 09:45:56 +0800

    docs: update readme & code comment

--- README.md
+++ README.md
@@ -1,6 +1,74 @@
-"https://hub.uuuadc.top", 
-"https://docker.anyhub.us.kg", 
-"https://dockerhub.jobcher.com", 
-"https://dockerhub.icu", 
-"https://docker.ckyl.me", 
-"https://docker.awsl9527.cn"
+
+
+
+# 视频流播放服务
+
+
+### 部署使用
+
+``` bash
+
+$ docker-compose up -d
+```
+
+
+### 功能描述
+
+基于github 开源项目 。通过 node server 转发 url参数向流媒体发送添加视频推流的请求。然后在前端播放 flv 页面
+
+前端可以通过 `iframe` `webview` 等技术通过配置url参数即可。
+
+e.g. `http://192.168.2.248:9300/?stream=YOUR_STREAM_NAME&url=rtsp%3A%2F%2FRTSP_USERNAME%3ARTSP_PASSWORD%40192.168.2.7%3A554%2FStreaming%2FChannels%2F2`
+
+- stream: 应用名, 重复的应用名只会返回同一个视频流，请自己在前端管理好视频流名称
+- url: 视频流原始地址 需要转码 `encodeURIComponent(origin_url)`
+
+
+### 项目结构 
+
+
+``` bash
+- server/ # node 转发服务
+  - public/ # 前端视频画面
+- docker-compose.yml # 项目配置 
+```
+
+
+### 开发
+
+``` bash
+$ docker-compose up --build
+```
+
+
+
+### tips 
+
+
+1. docker 镜像源 
+
+
+- linux: `vi /etc/docker/daemon.json`
+- windows: 打开桌面应用 `Settings` - `Docker Engine`
+- 修改: registry-mirrors
+
+``` json
+{
+  "registry-mirrors": [
+    "https://hub.uuuadc.top", 
+    "https://docker.anyhub.us.kg", 
+    "https://dockerhub.jobcher.com", 
+    "https://dockerhub.icu", 
+    "https://docker.ckyl.me", 
+    "https://docker.awsl9527.cn"
+  ]
+}
+
+```
+
+- 重启 docker
+
+``` bash
+$ systemctl daemon-reload
+$ systemctl reload docker
+```
--- server/index.mjs
+++ server/index.mjs
@@ -4,24 +4,26 @@ import fs from 'fs'
 import { mediaApi } from './api/index.mjs'
 const app = express()
 const port = Number(process.env.SERVER_PORT)
-const mediaSevicePort = Number(process.env.MEDIA_SERVICE_PORT)
+const mediaServicePort = Number(process.env.MEDIA_SERVICE_PORT)
 
 let mediaSecret = ''
 function getMediaSecret() {
   if (!mediaSecret) {
-    const mediaConf = fs.readFileSync('/opt/media/conf/config.ini', { encoding: 'utf-8' })
+    const mediaConf = fs.readFileSync(process.env.MEDIA_SERVICE_CONFIG_URI, { encoding: 'utf-8' })
     mediaSecret = mediaConf.match(/(?<=secret=)[a-zA-Z\d]+/g)?.[0] ?? ''
   }
   return mediaSecret
 }
 
-
+// 初始化 ejs 模版引擎, 指定模版目录
 app.set('view engine', 'ejs')
 app.set('views', './public')
 
+// 初始化静态文件目录
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
+// 处理业务逻辑
 app.get('/', function (req, res) {
   const { stream, url } = req.query
   console.log(stream, url)
@@ -30,20 +32,30 @@ app.get('/', function (req, res) {
   mediaApi.get('/addStreamProxy', {
     params: {
       secret: getMediaSecret(),
-      vhost: '__defaultVhost__',  // e.g. http://localhost:8080/${app}/${stream}.live.flv
+      // e.g. http://localhost:8080/${app}/${stream}.live.flv
+      vhost: '__defaultVhost__',  
+      // 根据业务模型设计复杂度，这里只通过 stream 进行区别
       app: 'live',
       stream: stream,
       url: url,
     }
   })
+  // 无论是否添加成功，均向前端返回画面 /public/index.mjs
   .finally(() => {
+    
     res.render('index', {
       pageTitle: 'video',
-      videoPort: mediaSevicePort,
+      videoPort: mediaServicePort,
       videoStream: stream
     })
   })
 })
+
+
+// 下方为测试代码 
+//
+//
+//
 
 app.get('/config', (req, res) => {
 
